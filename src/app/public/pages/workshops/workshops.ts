6d732f7b0ea0c24e1300d5c96b5d8fd1a028cc04
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2025-08-10 12:31:01 +0200

    Subida a producción

--- src/app/public/pages/workshops/workshops.ts
+++ src/app/public/pages/workshops/workshops.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Curso } from '../../../models/curso.model';
+import { CursoService } from '../../../services/curso/curso';
 
 @Component({
   selector: 'app-workshops',
@@ -9,4 +11,37 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./workshops.scss'],
   imports: [CommonModule, RouterModule]
 })
-export class Workshops {}
+export class Workshops implements OnInit {
+  
+  cursos: Curso[] = [];
+
+  constructor(private cursoService: CursoService) {}
+
+  ngOnInit(): void {
+    this.cargarCursos();
+  }
+
+  private cargarCursos(): void {
+    this.cursoService.getCursos().subscribe({
+      next: (data) => {
+        this.cursos = data;
+        this.cursos.forEach(curso => this.procesarImagenesBase64(curso));
+      },
+      error: (err) => console.error('Error cargando cursos', err),
+    });
+  }
+
+  private procesarImagenesBase64(curso: Curso): void {
+    for (let i = 1; i <= 5; i++) {
+      const imgProp = `img${i}` as keyof Curso;
+      const urlProp = `img${i}Url` as keyof Curso;
+
+      const base64Str = curso[imgProp] as unknown as string;
+      if (base64Str) {
+        (curso as any)[urlProp] = `data:image/webp;base64,${base64Str}`;
+      } else {
+        (curso as any)[urlProp] = '';
+      }
+    }
+  }
+}
