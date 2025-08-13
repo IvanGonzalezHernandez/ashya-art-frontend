cd525a4c8747fa01479689e84343b9b63ad87faf
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2025-08-13 20:02:22 +0200

    Añadiendo secciones de tarjeta regalo, firing service y remodelacion de about y studio

--- src/app/public/pages/gift-cards/gift-cards.ts
+++ src/app/public/pages/gift-cards/gift-cards.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TarjetaRegalo } from '../../../models/tarjetaRegalo.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TarjetaRegaloService } from '../../../services/tarjetaRegalo/tarjetaRegalo';
 
 @Component({
   selector: 'app-gift-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './gift-cards.html',
-  styleUrl: './gift-cards.scss'
+  styleUrls: ['./gift-cards.scss']
 })
-export class GiftCards {
+export class GiftCards implements OnInit {
+  loading = false;
+  tarjetasCargadas = false;
 
+  tarjetas: TarjetaRegalo[] = [];
+
+  constructor(private tarjetaRegaloService: TarjetaRegaloService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.cargarTarjetas();
+  }
+
+  private cargarTarjetas(): void {
+    this.tarjetaRegaloService.getTarjetas().subscribe({
+      next: (data) => {
+        this.tarjetas = data;
+        this.tarjetasCargadas = true;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error cargando tarjetas regalo', err);
+        this.tarjetasCargadas = true;
+        this.loading = false;
+      }
+    });
+  }
 }
