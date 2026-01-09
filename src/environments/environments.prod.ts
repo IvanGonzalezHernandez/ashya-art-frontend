002366b127a549b3e64f489dde01938431235a50
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2026-01-09 21:34:03 +0100

    Quitando pantalla de mantenimiento

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://ashya-art-backend-pro.onrender.com/api',
   gaMeasurementId: 'G-R2F37JR5WR', //G-JWRM8FGEV9 (esta es mi API, la que está activa es la de Ashya)
-  maintenance: true,              
+  maintenance: false,              
   maintenancePassword: 'admin123'
 };
